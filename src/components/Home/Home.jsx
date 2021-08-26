f6742bbc422081c8f7d1e704fa7c21f3c21bbdf6
Luciobio <pardobio@gmail.com>
2021-08-26 13:54:35 -0300

    fixed background

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,6 +7,11 @@ import logo from '../../icons/3DUS_icon.png'
 const Home = () => {
     return (
         <div className='home'>
+            <img 
+                className='home-bkg'
+                src='https://3dus.us/wp-content/uploads/2021/05/Marina-Westshore-Townhomes-Aerial-view-scaled.jpg'
+                alt='home-bkg'
+            />
             <div className='intro-poster'>
                 <img src={logo} className='logo' alt="Logo"/>
                 <h1 className='intro-title'>Building Visualizer</h1>
