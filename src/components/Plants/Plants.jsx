395ebae6bdb0de86d40fe00dbc8b001c38c29640
Luciobio <pardobio@gmail.com>
2021-08-30 13:01:57 -0300

    plants images added

--- src/components/Plants/Plants.jsx
+++ src/components/Plants/Plants.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
+import Slideshow from '../Slideshow/Slideshow'
+import terrace from './../../images/floorplans/01-terrace.jpg'
+import thirdfloor from './../../images/floorplans/02-3rd_floor.jpg'
+import secondfloor from './../../images/floorplans/03-2nd_floor.jpg'
+import firstfloor from './../../images/floorplans/04-1st_floor.jpg'
 import './Plants.css'
 
+const plants = [
+    {
+      id:0,
+      name:'Terrace',
+      src: terrace
+    },
+    {
+      id:1,
+      name: 'Third Floor',
+      src: thirdfloor
+    },
+    {
+      id:2,
+      name: 'Second Floor',
+      src: secondfloor
+    },
+    {
+      id:3,
+      name: 'First Floor',
+      src: firstfloor
+    }
+  ];
+
 const Plants = () => {
-    return (
-        <div className='plants'>
-            <h1>Plants</h1>
-        </div>
-    )
-}
+  
+  return (
+    <div>
+      <Slideshow items = {plants}/>
+    </div>
+  );
+  }
 
 export default Plants
