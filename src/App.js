f756e359a42d1616796f079a7285f7537f2e9820
Luciobio <pardobio@gmail.com>
2021-08-31 16:33:18 -0300

    nice menu ready

--- src/App.js
+++ src/App.js
@@ -9,26 +9,69 @@ import Home from './components/Home/Home'
 import Building from './components/Building/Building'
 import Amenities from './components/Amenities/Amenities'
 import Plants from './components/Plants/Plants'
+import {ReactComponent as MenuIcon} from './icons/menu.svg'
+import {ReactComponent as Xmark} from './icons/x-mark.svg'
+import blackLogo from './icons/3DUS_logo_black.png'
+import {ReactComponent as FbIcon} from './icons/facebook.svg'
+import {ReactComponent as IgIcon} from './icons/instagram.svg'
+import {ReactComponent as WaIcon} from './icons/whatsapp.svg'
 
 import './App.css'
 
+
 function App() {
+
+  const date = new Date("01/20/2021");
+  const year = date.getFullYear();
+
+  
+
   return (
     <Router className='main'>
       <div className='App'>
-      <div className='menu'>
-        <Link to='/' className='btn'>
-          Home
-        </Link>
-        <Link to='/building' className='btn'>
-          Building
-        </Link>
-        <Link to='/amenities' className='btn'>
-          Amenities
-        </Link>
-        <Link to='/plants' className='btn'>
-          Plants
-        </Link>
+      <div className='menu-cont'>
+        <button className='menu-btn'>
+          <MenuIcon className='menuIcon'/>
+        </button>
+        <div className='menu'>
+          <div className='menu-head'>
+            <button className='btn'>
+              <Xmark className='xmark' />
+            </button>
+          </div>
+          <div className='menu-links'>
+            <Link to='/' className='menu-link'>
+              Home
+            </Link>
+            <Link to='/building' className='menu-link'>
+              Building
+            </Link>
+            <Link to='/amenities' className='menu-link'>
+              Amenities
+            </Link>
+            <Link to='/plants' className='menu-link'>
+              Plants
+            </Link>
+          </div>
+          <div className='menu-bot'>
+            <img src={blackLogo} className='menu-logo' alt=' '/>
+            <h2>3DUS</h2>
+            <div className='mediaIcons'>
+              <a className='web-link'href='https://www.facebook.com/3dfactoryUS/'>
+                <FbIcon className='media-icon'/>
+              </a>
+              <a className='web-link'href='https://www.instagram.com/3dfactoryus/'>
+                <IgIcon className='media-icon'/>
+              </a>
+              <a className='web-link'href='https://wa.me/18139658628'>
+                <WaIcon className='media-icon'/>
+              </a>
+            </div>  
+            <div className='copywrite'>
+              <p className='web-link'>{year} © <a className='web-link'href='https://3dus.us'>3DUS.us</a></p>
+            </div>
+          </div>
+        </div>
       </div>
       <Switch>
         <Route path='/' exact>
